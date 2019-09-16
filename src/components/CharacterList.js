d3a9d02d8c1ca3f13bd931b52bd9c5a51f3d2ceb
Romairo1987 <49536760+Romairo1987@users.noreply.github.com>
2019-09-15 18:26:07 -0700

    not-completed

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,41 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {Route,Link} from "react-router-dom";
 
-export default function CharacterList() {
+
+// import src from "*.svg";
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
-
+  const [characters,setCharacters]=useState([])
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get("https://rickandmortyapi.com/api/character/")
+    .then(res=>{
+      // console.log(res)
+      setCharacters(res.data.results)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
   }, []);
 
   return (
     <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
+        {characters.map(char=>(
+          <div key={char.id}>
+            <img src={char.image} alt="character image"/>
+            <h2>{char.name}</h2>
+            <h5>{char.species} {char.status}</h5>
+            <h4> {`Location : ${char.location.name}`}</h4>
+            <h4> {`Origin : ${char.location.name}`}</h4>
+            <h4>Episodes</h4>
+            <Link to={`/characterlist/${char.id}`}>{char.name}</Link>
+          </div>
+         
+        ))}  
+        
+        
     </section>
   );
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,29 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function LocationsList() {}
+export default function LocationsList() {
+    const [locations, setLocations]=useState([])
+    useEffect(()=>{
+        axios.get("https://rickandmortyapi.com/api/location/")
+        .then(res=>{
+            console.log(res)
+            setLocations(res.data.results)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    },[])
+    
+    return(
+        <section className="Location-list">
+            {locations.map(loc=>(
+                <div key={loc.id}>
+                    
+                    <h3>{loc.name}</h3>
+                    <h4>{loc.type}</h4>
+                    <h5>{`${loc.residents.length} resdients`}</h5>
+                </div>
+            ))}
+        </section>
+    )
+}
